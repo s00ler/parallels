44c622156d7e09cac5eec1807fed0d60087e60e4
s00ler <d.a.gerasimov.94@gmail.com>
2017-11-25 02:50:27 +0300

    test C++11

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <vector>
+
 
 int main(int argc, char const *argv[]) {
-    std::cout << "Hello world!" << '\n';
-    return 0;
+        std::cout << "Hello world!" << '\n';
+        std::vector<int> v;
+        v.push_back(1);
+        v.push_back(1);
+        v.push_back(1);
+        v.push_back(1);
+        for (auto i: v) {
+                std::cout << i + 1 << '\n';
+        }
+
+        return 0;
 }
