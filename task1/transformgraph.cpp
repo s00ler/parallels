50be89b6c830517e5a1e7685f24767ee8dbf1eb8
s00ler <d.a.gerasimov.94@gmail.com>
2017-12-01 20:18:29 +0300

    add weight flag

--- task1/transformgraph.cpp
+++ task1/transformgraph.cpp
@@ -9,16 +9,24 @@ int main(int argc, char const *argv[]) {
         if (argc < 3) {
                 cout << "Not enough arguments." << '\n';
                 cout << "Right way to use program:" << '\n';
-                cout << argv[0] << " " << "source_graph_name " << "resulting_graph_name"<< '\n';
+                cout << argv[0] << " " << "source_graph_name " << "resulting_graph_name"<< "1/0" <<'\n';
                 return 0;
         }
+
+        if (argc == 4) {
+                int weighted = atoi(argv[3]);}
+        else {
+                int weighted = 1
+        }
+
         int vertices_count = 0;
         long long edges_count = 0;
         string new_name = argv[2];
         new_name = new_name + ".weights";
         fstream source(argv[1], ios::in | ios::binary);
         fstream edges(argv[2], ios::out | ios::binary);
-        fstream weights(new_name, ios::out | ios::binary);
+        if (weighted == 1) {
+                fstream weights(new_name, ios::out | ios::binary);}
 
         source.read((char*)(&vertices_count), sizeof(int));
         source.read((char*)(&edges_count), sizeof(long long));
@@ -30,12 +38,15 @@ int main(int argc, char const *argv[]) {
                 // read i-th edge data
                 source.read((char*)(&src_id), sizeof(int));
                 source.read((char*)(&dst_id), sizeof(int));
-                source.read((char*)(&weight), sizeof(float)); // remove it for unweighed graph
+                if (weighted == 1) {
+                        source.read((char*)(&weight), sizeof(float));}
                 // write i-th edge data
                 edges.write((char*)(&src_id), sizeof(int));
                 edges.write((char*)(&dst_id), sizeof(int));
                 edges.write((char*)(&ph), sizeof(int));
-                weights.write((char*)(&weight), sizeof(float));  // remove it for unweighed graph
+                if (weighted == 1) {
+                        weights.write((char*)(&weight), sizeof(float));
+                }
         }
         source.close();
         edges.close();
