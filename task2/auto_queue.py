86aa91b19fea3ef53c210c1c48ba2a5b65095425
s00ler <d.a.gerasimov.94@gmail.com>
2017-12-18 02:59:15 +0300

    mod auto_queue for python 2

--- task2/auto_queue.py
+++ task2/auto_queue.py
@@ -9,20 +9,20 @@ tasks = []
 for procs in processes:
     for grid_size in grid_sizes:
         tasks.append((procs, grid_size))
-subprocess.check_output(['bash', '-c', "module add slurm"]).decode()
+
 while tasks:
     bashCommand = "squeue -p test"
-    output = subprocess.check_output(['bash', '-c', bashCommand]).decode()
+    output = subprocess.Popen(bashCommand.split(' '), stdout=subprocess.PIPE).communicate()[0]
     output = list(map(lambda l: l.split(' '), output.split('\n')))
     output = [line for line in output if 'dagerasi' in line]
-    free_spaces = 3 - len(output) + 1
+    free_spaces = 3 - len(output)
     for i in range(free_spaces):
         task = tasks.pop()
-        launchcmd = "sbatch -p test -n {} ./mpitask2 {} 0".format(task[0],
-                                                                  task[1])
-        output = subprocess.check_output(['bash', '-c', launchcmd]).decode()
+        launchcmd = "sbatch -p test -n {0} impi ./mpitask2 {1} 0".format(task[0],
+                                                                         task[1])
+        output = subprocess.Popen(launchcmd.split(' '), stdout=subprocess.PIPE).communicate()[0]
         print(output)
         time.sleep(5)
-    time.sleep(10)
+    time.sleep(30)
 
 print("All tasks launched!")
